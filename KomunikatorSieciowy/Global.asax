f04c1d1b67dd9009f0b02e9e3193e79b8905db2f
wczerniakowski <wojtek1093@gmail.com>
2015-09-01 17:50:24 +0200

    Komunikator Sieciowy

---
+++ KomunikatorSieciowy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KomunikatorSieciowy.MvcApplication" Language="C#" %>
---
+++ KomunikatorSieciowy/StartUp/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(KomunikatorSieciowy.StartUp.Startup))]
+
+namespace KomunikatorSieciowy.StartUp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
